2a4e2913b6f0f007bad24a7674b6e7d65675ee59
johanna <johannak12@gmail.com>
2018-03-17 12:21:18 +0100

    Add empty index result condition

--- SolveRPM2.m
+++ SolveRPM2.m
@@ -8,8 +8,6 @@ function [dv, FVAL, pi, sigma] = solveRPM2(dv,recap_rate,delta,num_flights,num_i
 %__________________________________________________________________________
 
 
-
-
 %TODO: also return index of variables in matrix!
     numdv=length(dv(:,1)); 
     % Add Columns:
@@ -35,40 +33,42 @@ function [dv, FVAL, pi, sigma] = solveRPM2(dv,recap_rate,delta,num_flights,num_i
     Aineq = zeros(num_flights,numdv);
     rhs   = zeros(num_flights,1);
     
-% %     %Constraints (6)
-% %     %TODO: Two inner loops can probably be made into one
-% % %     C6=zeros(num_flights,numdv); 
-% % %     for f=1:num_flights
-% % %         for p6=1:num_it
-% % %             for r6=1:num_it
-% % %                 recap_index = find(r==r6 & p==p6); %Find right index for dv 
-% % %                 dv_index = find(dv(:,1)==recap_index); 
-% % %                 if isempty(dv_index)
-% % %                     continue
-% % %                 else 
-% % %                     C6(dv_index)= -1* delta(f,p6); %-1 because >= constraint
-% % %                 end  
-% % %             end   
-% % %         end   
-% % %         
-% % %         for r6=1:num_it
-% % %             for p6=1:num_it
-% % %                 recap_index = find(r==p6 & p==r6); 
-% % %                 dv_index = find(dv(:,1)==recap_index); 
-% % %                 if isempty(dv_index)
-% % %                     continue 
-% % %                 else 
-% % %                     C6(dv_index)= -1 * -1*b(recap_index);  %-1 because >= constraint and in constraint
-% % %                 end        
-% % %             end
-% % %         end 
-% % %            
-% % %     end
-% % %     %Because constraint >= multiply by -1 
-% % %     rhs6=delta*it(:,2)-capacity; %Not sure if right?!  --> right ;)
-% % %     rhs6 =rhs6*-1;
-% % %     rhs=rhs6;
-% % %     Aineq=C6; 
+    %Constraints (6)
+    %TODO: Two inner loops can probably be made into one
+    C6=zeros(num_flights,numdv); 
+    for f=1:num_flights
+        for p6=1:num_it
+            for r6=1:num_it
+                recap_index = find(r==r6 & p==p6); %Find right index for dv
+                if isempty(recap_index)
+                    continue 
+                else
+                    dv_index = find(dv(:,1)==recap_index);
+                    C6(dv_index)= C6(dv_index) -1* delta(f,p6); %-1 because >= constraint
+                end     
+            end   
+        end   
+        
+        for r6=1:num_it
+            for p6=1:num_it
+                recap_index = find(r==p6 & p==r6); 
+                if isempty(recap_index)
+                    continue
+                else
+                    dv_index = find(dv(:,1)==recap_index); 
+                    C6(dv_index)= C6(dv_index) -1 * -1*b(recap_index);  %-1 because >= constraint and in constraint      
+                end  
+            end
+        end 
+           
+    end
+    disp('columns 6 added')
+    
+    %Because constraint >= multiply by -1 
+    rhs6=delta*it(:,2)-capacity; %Not sure if right?!  --> right ;)
+    rhs6 =rhs6*-1;
+    rhs=rhs6;
+    Aineq=C6; 
  
 
     %Constraints (7)
@@ -77,6 +77,7 @@ function [dv, FVAL, pi, sigma] = solveRPM2(dv,recap_rate,delta,num_flights,num_i
     % Add rows:        
     Aineq=[Aineq;Aineq7(v_addrow,:)];
     rhs = [rhs;rhs7(v_addrow)];
+    disp('constraints 7 added')
     
     lb = zeros(numdv,1); 
     ub = []; 
